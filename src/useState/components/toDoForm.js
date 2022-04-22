41090694a542844ae3c4f1122fc1e9df43c1d493
No Name <dev.haxuan@gmail.com>
2022-04-23 01:06:35 +0700

    todo form

--- src/useState/components/toDoForm.js
+++ src/useState/components/toDoForm.js
@@ -1,26 +1,31 @@
-import React, {useState}  from "react";
+import React, { useState } from "react";
 
-const TodoForm = ({onSumbit}) => {
+const TodoForm = ({ onSumbit }) => {
     const [value, setValue] = useState('');
     const handleValueChange = (e) => {
         setValue(e.target.value);
     }
     const handleSumbit = (e) => {
-        e.preventDefault();
+        e.preventDefault(); // chặn trình duyệt tự động reload
         if (!onSumbit) return;
         const formValues = {
             title: value,
-            //...
+            //...sau có trường khác thì điền thêm.
         }
         onSumbit(formValues);
 
-        //Reset input
+        //Reset value
         setValue('');
     }
     return (
-      <form onSubmit={handleSumbit}>
-          <input type={'text'} value={value} onChange={handleValueChange} />
-      </form>
+        <form onSubmit={handleSumbit}>
+            <input
+                type={'text'}
+                value={value}
+                onChange={handleValueChange}
+                placeholder='Enter Name...'
+            />
+        </form>
     )
 }
 export default TodoForm;
--- src/useState/toDoList.js
+++ src/useState/toDoList.js
@@ -3,33 +3,33 @@ import TodoForm from './components/toDoForm';
 import { ToDoList } from './components/toDoList';
 
 const ToDo = () => {
-    const [toDoList, setTodoList] = useState([
-        {id: 1, title: 'No Name - Ha Van Xuan - 1'},
-        {id: 2, title: 'No Name - Ha Van Xuan - 2'},
-        {id: 3, title: 'No Name - Ha Van Xuan - 3'}
+    const [todoList, setTodoList] = useState([
+        { id: 1, title: 'No Name - Ha Van Xuan - 1' },
+        { id: 2, title: 'No Name - Ha Van Xuan - 2' },
+        { id: 3, title: 'No Name - Ha Van Xuan - 3' }
     ]);
-    console.log(toDoList);
-    const handleClick = (todo) => {
-       const index = toDoList.findIndex(item => item.id === todo.id);
-       if (index < 0) return;
-       let newTodo = [...toDoList];
-       newTodo.splice(index, 1);
+    console.log(todoList);
+    const handleRemoveClick = (todo) => {
+        const index = todoList.findIndex(item => item.id === todo.id);
+        if (index < 0) return;
+        let newTodo = [...todoList];
+        newTodo.splice(index, 1);
         setTodoList(newTodo);
     }
-    const handleTodoFormSumbit= (formValues) => {
+    const handleTodoFormSumbit = (formValues) => {
         const newTodo = {
-            id: toDoList.length + 1,
+            id: todoList.length + 1,
             ...formValues,
         }
-        const newTodoList = [...toDoList];
+        const newTodoList = [...todoList];
         newTodoList.push(newTodo);
         setTodoList(newTodoList);
     }
 
     return (
         <>
-        <TodoForm onSumbit={handleTodoFormSumbit}/>
-        <ToDoList todos={toDoList} onToDoClick={handleClick} />
+            <TodoForm onSumbit={handleTodoFormSumbit} />
+            <ToDoList todos={todoList} onToDoClick={handleRemoveClick} />
         </>
     )
 }
